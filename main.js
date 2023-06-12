bce09756545dfdd6542d45c997a37d000f2ddd2c
Nhlamuloj <nmahlabana@gmail.com>
2023-06-12 13:15:48 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// toggle icon
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onClick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+
+
+//scroll section
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop-100;
+        let height = sec.offsetHight;
+        let id = sec.getAttribute('id');
+
+        if( top >= offset && top < offset + height){
+            //active navbar links
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+            });
+        }
+    });
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY>100);
+}
+
